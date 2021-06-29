e5c786fedef0882f763806ba91c8f9055396151d
Retch <retch100@gmail.com>
2021-06-29 15:56:19 +0200

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const db = require('./app/config/db.config.js');
 const expressport = 8000;
 
 // force: true will drop the table if it already exists
-const resync = false;
+const resync = true;
 if (resync) {
   db.sequelize.sync({force: true}).then(() => {
     console.log('Drop and Resync with { force: true }');
